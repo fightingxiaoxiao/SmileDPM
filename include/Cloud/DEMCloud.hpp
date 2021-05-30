9999dcdc7edefffa73b3969e8ce167caefd89e13
Xiaoxiao <smilemax@foxmail.com>
2021-05-30 18:34:56 +0800

    修复包含路径

---
+++ include/Cloud/DEMCloud.hpp
@@ -0,0 +1,28 @@
+#ifndef DEMCLOUD_HPP
+#define DEMCLOUD_HPP
+
+#include "Cloud.hpp"
+
+namespace Smile
+{   
+    template <int dim>
+    class DEMCloud
+    {
+    private:
+        /* data */
+    public:
+        DEMCloud(/* args */);
+        ~DEMCloud();
+    };
+    
+    DEMCloud::DEMCloud(/* args */)
+    {
+    }
+    
+    DEMCloud::~DEMCloud()
+    {
+    }
+    
+} // namespace Smile
+
+#endif
